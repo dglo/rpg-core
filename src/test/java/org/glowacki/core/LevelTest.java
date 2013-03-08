37c69de91a1fc23e0a4d43d2a51696fb9233d998
Dave Glowacki <dave@glowacki.org>
2013-03-08 13:02:12 -0600

    Add more tests

--- src/test/java/org/glowacki/core/LevelTest.java
+++ src/test/java/org/glowacki/core/LevelTest.java
@@ -18,6 +18,26 @@ public class LevelTest
         return new TestSuite(LevelTest.class);
     }
 
+    public void testCreate()
+        throws CoreException
+    {
+        final String name = "Create";
+        final String[] map = new String[] {
+            "---",
+            "|.|",
+            "---",
+        };
+
+        Level lvl = new Level(name, map);
+        assertEquals("Bad name", name, lvl.getName());
+        assertEquals("Bad max X", lvl.getMaxX(), map[0].length() - 1);
+        assertEquals("Bad max Y", lvl.getMaxY(), map.length - 1);
+        assertNotNull("Null character list", lvl.getCharacters());
+        assertEquals("Non-empty character list",
+                     lvl.getCharacters().size(), 0);
+        assertNotNull("Null string", lvl.toString());
+    }
+
     public void testBuild()
         throws CoreException
     {
@@ -41,6 +61,46 @@ public class LevelTest
 
             assertEquals("Bad line #" + i, mtrim, ptrim);
         }
+    }
+
+    public void testMultiLevel()
+        throws CoreException
+    {
+        String[] map1 = new String[] {
+            "-----",
+            "|...|",
+            "|.>.|",
+            "|...|",
+            "-----",
+        };
+
+        String[] map2 = new String[] {
+            "-----",
+            "|...|",
+            "|.<.|",
+            "|...|",
+            "-----",
+        };
+
+        Level l1 = new Level("1", map1);
+        Level l2 = new Level("2", map2);
+
+        assertNull("Next level for level 1 is not null", l1.getNextLevel());
+        assertNull("Previous level for level 1 is not null",
+                   l1.getPreviousLevel());
+        assertNull("Next level for level 2 is not null", l2.getNextLevel());
+        assertNull("Previous level for level 2 is not null",
+                   l2.getPreviousLevel());
+
+        l1.addNextLevel(l2);
+
+        assertEquals("Bad next level for level 1",
+                     l1.getNextLevel(), l2);
+        assertNull("Previous level for level 1 is not null",
+                   l1.getPreviousLevel());
+        assertNull("Next level for level 2 is not null", l2.getNextLevel());
+        assertEquals("Bad previous level for level 2",
+                   l2.getPreviousLevel(), l1);
     }
 
     public static void main(String[] args)
