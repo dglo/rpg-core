6cd6c1ead69beb20aac1654d7e417fd612b82757
Dave Glowacki <dave@glowacki.org>
2013-03-09 18:21:39 -0600

    Test LevelCharacter

--- src/test/java/org/glowacki/core/LevelTest.java
+++ src/test/java/org/glowacki/core/LevelTest.java
@@ -58,6 +58,63 @@ public class LevelTest
         super(name);
     }
 
+    private String[] buildMap(int upX, int upY, int downX, int downY)
+        throws CoreException
+    {
+        if (upX == 0) {
+            throw new CoreException("Up X " + upX +
+                                    " would be embedded in the wall");
+        } else if (downX == 0) {
+            throw new CoreException("Down X " + downX +
+                                    " would be embedded in the wall");
+        } else if (upY == 0) {
+            throw new CoreException("Up Y " + upY +
+                                    " would be embedded in the wall");
+        } else if (downY == 0) {
+            throw new CoreException("Down Y " + downY +
+                                    " would be embedded in the wall");
+        } else if (upX == downX && upY == downY) {
+            final String fmt = "Up (%d,%d) and down (%d,%d) are the same";
+            throw new CoreException(String.format(fmt, upX, upY, downX, downY));
+        }
+
+        int maxX = (upX > downX ? upX : downX);
+        int maxY = (upY > downY ? upY : downY);
+
+        String[] map = new String[maxY + 3];
+
+        final String wallFmt = String.format("%%%ds", maxX + 3);
+        final String allWall = String.format(wallFmt, "").replace(' ', '-');
+
+        StringBuilder buf = new StringBuilder(maxX + 3);
+        for (int i = 0; i < map.length; i++) {
+            if (i == 0 || i == map.length - 1) {
+                map[i] = allWall;
+                continue;
+            }
+
+            buf.setLength(0);
+            buf.append('|');
+
+            int floorEnd = maxX + 1;
+            for (int j = 1; j < maxX + 2; j++) {
+                if (i == upY && upX == j) {
+                    buf.append('<');
+                } else if (i == downY && downX == j) {
+                    buf.append('>');
+                } else {
+                    buf.append('.');
+                }
+            }
+
+            buf.append('|');
+
+            map[i] = buf.toString();
+        }
+
+        return map;
+    }
+
     public static Test suite()
     {
         return new TestSuite(LevelTest.class);
@@ -154,9 +211,29 @@ public class LevelTest
         }
     }
 
-    public void testMultiLevel()
+    public void testBuildMap()
         throws CoreException
     {
+        String[] map = new String[] {
+            "------",
+            "|....|",
+            "|.<>.|",
+            "|....|",
+            "------",
+        };
+
+        final int row = 2;
+        final int upX = 2;
+        final int downX = 3;
+
+        String[] built = buildMap(upX, row, downX, row);
+        assertNotNull("buildMap() returned null", built);
+        assertEquals("Bad map size", map.length, built.length);
+
+        for (int i = 0; i < map.length; i++) {
+            assertEquals("Bad map line #" + i, map[i], built[i]);
+        }
+
         String[] map1 = new String[] {
             "-----",
             "|...|",
@@ -164,6 +241,14 @@ public class LevelTest
             "|...|",
             "-----",
         };
+
+        String[] built1 = buildMap(-1, -1, 2, 2);
+        assertNotNull("buildMap() returned null", built1);
+        assertEquals("Bad map size", map1.length, built1.length);
+
+        for (int i = 0; i < map1.length; i++) {
+            assertEquals("Bad map line #" + i, map1[i], built1[i]);
+        }
 
         String[] map2 = new String[] {
             "-----",
@@ -173,7 +258,19 @@ public class LevelTest
             "-----",
         };
 
-        Level l1 = new Level("1", map1);
+        String[] built2 = buildMap(2, 2, -1, -1);
+        assertNotNull("buildMap() returned null", built2);
+        assertEquals("Bad map size", map2.length, built2.length);
+
+        for (int i = 0; i < map2.length; i++) {
+            assertEquals("Bad map line #" + i, map2[i], built2[i]);
+        }
+    }
+
+    public void testMultiLevel()
+        throws CoreException
+    {
+        Level l1 = new Level("1", buildMap(-1, -1, 2, 2));
 
         try {
             l1.addNextLevel(null);
@@ -184,7 +281,7 @@ public class LevelTest
                          "Next level cannot be null", ce.getMessage());
         }
 
-        Level l2 = new Level("2", map2);
+        Level l2 = new Level("2", buildMap(2, 2, -1, -1));
 
         assertNull("Next level for level 1 is not null", l1.getNextLevel());
         assertNull("Previous level for level 1 is not null",
@@ -203,7 +300,7 @@ public class LevelTest
         assertEquals("Bad previous level for level 2",
                      l1, l2.getPreviousLevel());
 
-        Level l3 = new Level("Extra", map2);
+        Level l3 = new Level("Extra", buildMap(4, 3, 2, 1));
 
         try {
             l1.addNextLevel(l3);
@@ -294,22 +391,14 @@ public class LevelTest
         }
     }
 
-    public void testEnterExit()
+    public void testEnter()
         throws CoreException
     {
-        String[] map = new String[] {
-            "------",
-            "|....|",
-            "|.<>.|",
-            "|....|",
-            "------",
-        };
-
         final int row = 2;
         final int upX = 2;
         final int downX = 3;
 
-        Level lvl = new Level("enterExit", map);
+        Level lvl = new Level("enterExit", buildMap(upX, row, downX, row));
         assertNotNull("Null character list", lvl.getCharacters());
         assertEquals("Non-empty character list",
                      0, lvl.getCharacters().size());
@@ -382,6 +471,141 @@ public class LevelTest
             assertEquals("Bad exception message",
                          name + " was not on this level", ce.getMessage());
         }
+    }
+
+    public void testEnterChar()
+        throws CoreException
+    {
+        final int row = 2;
+        final int upX = 2;
+        final int downX = 3;
+
+        Level lvl = new Level("enterExit", buildMap(upX, row, downX, row));
+        assertNotNull("Null character list", lvl.getCharacters());
+        assertEquals("Non-empty character list",
+                     0, lvl.getCharacters().size());
+
+        Character real = new Character("joe", 3, 4, 5);
+
+        MovableCharacter ch = lvl.enterDown(real);
+        assertEquals("Bad X from enterDown", upX, ch.getX());
+        assertEquals("Bad Y from enterDown", row, ch.getY());
+        assertEquals("Bad character list", 1, lvl.getCharacters().size());
+
+        lvl.exit(ch);
+        assertEquals("Bad X after exit", -1, ch.getX());
+        assertEquals("Bad Y after exit", -1, ch.getY());
+        assertEquals("Non-empty character list",
+                     0, lvl.getCharacters().size());
+
+        lvl.enterUp(ch);
+        assertEquals("Bad X from enterDown", downX, ch.getX());
+        assertEquals("Bad Y from enterDown", row, ch.getY());
+        assertEquals("Empty character list", 1, lvl.getCharacters().size());
+
+        lvl.exit(ch);
+        assertEquals("Bad X after exit", -1, ch.getX());
+        assertEquals("Bad Y after exit", -1, ch.getY());
+        assertEquals("Non-empty character list",
+                     0, lvl.getCharacters().size());
+    }
+
+    public void testMoveChar()
+        throws CoreException
+    {
+        final int row = 1;
+        final int upX = 1;
+        final int downX = 2;
+
+        Level top = new Level("top", buildMap(upX, row, downX, row));
+        Level bottom = new Level("bottom", buildMap(upX, row, downX, row));
+        top.addNextLevel(bottom);
+
+        Character real = new Character("joe", 3, 4, 5);
+
+        MovableCharacter ch = top.enterDown(real);
+        assertEquals("Bad X from enterDown", upX, ch.getX());
+        assertEquals("Bad Y from enterDown", row, ch.getY());
+        assertEquals("Bad level from enterDown", top, ch.getLevel());
+        assertEquals("Bad initial character list",
+                     1, top.getCharacters().size());
+        assertEquals("Bad initial character list",
+                     0, bottom.getCharacters().size());
+
+        try {
+            ch.move(MovableCharacter.Direction.CLIMB);
+            fail("Should not be able to ascend from upper level");
+        } catch (CoreException ce) {
+            assertNotNull("Null exception message", ce.getMessage());
+            assertEquals("Bad exception message",
+                         "You cannot exit here", ce.getMessage());
+        }
+
+        try {
+            ch.move(MovableCharacter.Direction.DESCEND);
+            fail("Should not be able to descend from up staircase");
+        } catch (CoreException ce) {
+            assertNotNull("Null exception message", ce.getMessage());
+            assertEquals("Bad exception message",
+                         "You cannot descend here", ce.getMessage());
+        }
+
+        ch.move(MovableCharacter.Direction.LEFT);
+        assertEquals("Bad X after wall move", upX, ch.getX());
+        assertEquals("Bad Y after wall move", row, ch.getY());
+        assertEquals("Bad level after wall move", top, ch.getLevel());
+
+        ch.move(MovableCharacter.Direction.RIGHT);
+        assertEquals("Bad X after move right", downX, ch.getX());
+        assertEquals("Bad Y after move right", row, ch.getY());
+        assertEquals("Bad level after move right", top, ch.getLevel());
+
+        try {
+            ch.move(MovableCharacter.Direction.CLIMB);
+            fail("Should not be able to ascend from down staircase");
+        } catch (CoreException ce) {
+            assertNotNull("Null exception message", ce.getMessage());
+            assertEquals("Bad exception message",
+                         "You cannot climb here", ce.getMessage());
+        }
+
+        ch.move(MovableCharacter.Direction.DESCEND);
+        assertEquals("Bad X after descent", upX, ch.getX());
+        assertEquals("Bad Y after descent", row, ch.getY());
+        assertEquals("Bad level after descent", bottom, ch.getLevel());
+        assertEquals("Bad character list after descent",
+                     0, top.getCharacters().size());
+        assertEquals("Bad character list after descent",
+                     1, bottom.getCharacters().size());
+
+        ch.move(MovableCharacter.Direction.RIGHT);
+        assertEquals("Bad X after move right", downX, ch.getX());
+        assertEquals("Bad Y after move right", row, ch.getY());
+        assertEquals("Bad level after move right", bottom, ch.getLevel());
+
+        try {
+            ch.move(MovableCharacter.Direction.DESCEND);
+            fail("Should not be able to descend from lower level");
+        } catch (CoreException ce) {
+            assertNotNull("Null exception message", ce.getMessage());
+            assertEquals("Bad exception message",
+                         "You are at the bottom", ce.getMessage());
+        }
+
+        ch.move(MovableCharacter.Direction.LEFT);
+        assertEquals("Bad X after move left", upX, ch.getX());
+        assertEquals("Bad Y after move left", row, ch.getY());
+        assertEquals("Bad level after move left", bottom, ch.getLevel());
+
+        ch.move(MovableCharacter.Direction.CLIMB);
+        assertEquals("Bad X after ascent", downX, ch.getX());
+        assertEquals("Bad Y after ascent", row, ch.getY());
+        assertEquals("Bad level after ascent", top, ch.getLevel());
+        assertEquals("Bad character list after ascent",
+                     1, top.getCharacters().size());
+        assertEquals("Bad character list after ascent",
+                     0, bottom.getCharacters().size());
+
     }
 
     public static void main(String[] args)
