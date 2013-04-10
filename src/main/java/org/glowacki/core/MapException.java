0e9dfcba7761f913e220eb688a2aa00b57f1080b
Dave Glowacki <dave@glowacki.org>
2013-04-10 00:35:54 -0500

    Add javadoc

--- src/main/java/org/glowacki/core/MapException.java
+++ src/main/java/org/glowacki/core/MapException.java
@@ -6,11 +6,19 @@ package org.glowacki.core;
 public class MapException
     extends CoreException
 {
+    /**
+     * Create a map exception.
+     */
     public MapException()
     {
         super();
     }
 
+    /**
+     * Create a map exception.
+     *
+     * @param msg error message
+     */
     public MapException(String msg)
     {
         super(msg);
